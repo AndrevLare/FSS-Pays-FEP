2cb676c91f06b798149133e7e4b0cdcaa57ec0ef
Andrés Oviedo <90996615+AndrevLare@users.noreply.github.com>
2026-08-23 16:16:13 -0500

    Change label from 'Curso' to 'Grupo'

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ function crearTarjetaEstudiante(numero) {
         <span class="field-error"></span>
       </div>
       <div class="field full">
-        <label>Curso</label>
+        <label>Grupo</label>
         <input name="curso_${id}" placeholder="Ej: Kinder A">
       </div>
     </div>
